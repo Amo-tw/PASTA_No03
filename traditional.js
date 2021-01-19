bb54e8dce268af99e3317df1b148fe0219ac4a71
Amo Chen <t9525838@gmail.com>
2021-01-20 07:00:10 +0800

    add abbr_name block

--- traditional.js
+++ traditional.js
@@ -1,8 +1,6 @@
 document.write('<script src="menu.js"></script>');
 
 function renew() {
-  //alert(dishes[2].name);
-  //changeDish();
   num_of_dish();
 }
 
@@ -10,10 +8,6 @@ function getRandom(x) {
   return Math.floor(Math.random() * x) + 1;
 }
 function changeDish(amount_dish) {
-  //document.getElementById("order_no").innerHTML = amount_dish;
-
-  //document.getElementById("order_no").innerHTML = dishes[getRandom(35)].name;
-
   for (var i = 0; i < amount_dish; i++) {
     var this_dish = getRandom(35 - 1);
 
@@ -23,9 +17,12 @@ function changeDish(amount_dish) {
     document.getElementsByClassName("price_of_dish")[i].innerHTML =
       dishes[this_dish].price + " TWD";
 
+    document.getElementsByClassName("abbr_name")[i].innerHTML =
+      dishes[this_dish].abbr;
+    /*
     document.getElementsByClassName("extra")[i].innerHTML = "333";
     document.getElementsByClassName("customize")[i].innerHTML = "** 不要胡椒";
-        
+    */
   }
 }
 
@@ -33,15 +30,23 @@ function num_of_dish() {
   var amount_dish = getRandom(4);
 
   var one_dish =
-    '<div class="order_line"><div class="name_of_dish">預設</div><div class="price_of_dish"></div><div class="extra"></div><div class="customize"></div></div>';
+    '<div class="order_line"><div class="name_of_dish">預設</div><div class="price_of_dish"></div><div class="abbr_name"></div><div class="extra"></div><div class="customize"></div></div>';
 
   document.getElementById("order_block").innerHTML = "";
 
   for (var i = 0; i < amount_dish; i++) {
     document.getElementById("order_block").innerHTML += one_dish;
-    if(i<amount_dish-1)
-        document.getElementById("order_block").innerHTML += "<hr>";
+    if (i < amount_dish - 1)
+      document.getElementById("order_block").innerHTML += "<hr>";
   }
 
   changeDish(amount_dish);
+}
+
+function answer() {
+  var abb = document.getElementsByClassName("abbr_name");
+
+  for (var i = 0; i < abb.length; i++) {
+    abb[i].style.color = "red";
+  }
 }
